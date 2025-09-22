49a873c9888e1fe90923659306feaa56063ff95c
David Patrick <128710188+CyberPatrix-S3C@users.noreply.github.com>
2025-09-22 23:23:48 +0100

    Update tron-legacy.lua

--- color/tron-legacy.lua
+++ color/tron-legacy.lua
@@ -1,2 +1,11 @@
--- colors/tron-legacy.lua
-require("tron-legacy").load()
+-- File: tron-legacy.nvim/colors/tron-legacy.lua
+
+-- Always clear highlights before defining a new scheme
+vim.cmd("hi clear")
+
+if vim.fn.exists("syntax_on") then
+  vim.cmd("syntax reset")
+end
+
+-- REQUIRED: this registers the colorscheme name with Neovim
+vim.g.colors_name = "tron-legacy"
